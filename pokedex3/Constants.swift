e859c44ecb5ef9f6c7cb9ef38504ce0f1c723562
hamid9069 <hamid@Hamidrezas-MacBook-Pro.local>
2017-06-02 09:15:57 +0430

    App Completed

---
+++ pokedex3/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by Hamidreza Almasi on 6/2/17.
+//  Copyright © 2017 KTour. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
